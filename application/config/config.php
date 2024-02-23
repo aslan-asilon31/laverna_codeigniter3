b628fcffa6b74a5d345ddbd0bc1a02ed540da075
aslan-asilon31 <sulaslansetiawan31@gmail.com>
2024-02-22 23:24:13 -0800

    add landing page

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+// $config['base_url'] = ''; 
+
+// Get the protocol (http or https)
+$protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http';
+
+// Get the host
+$host = $_SERVER['HTTP_HOST'];
+
+// Get the base path
+$basePath = str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
+
+// Combine the above to create the base URL
+$config['base_url'] = $protocol . "://" . $host . $basePath;
+
 
 /*
 |--------------------------------------------------------------------------
